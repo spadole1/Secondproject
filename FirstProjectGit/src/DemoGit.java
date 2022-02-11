68b25d00e2d0d2333c566ed3dda7cd90a57954c6
spadole1 <99403235+spadole1@users.noreply.github.com>
2022-02-11 18:25:25 +0530

    Update DemoGit.java

--- FirstProjectGit/src/DemoGit.java
+++ FirstProjectGit/src/DemoGit.java
@@ -9,10 +9,13 @@ public class DemoGit {
 	m2()
 	{
 		System.out.println("program for m2 method");
+		System.out.println("program for check pull method");
 	}
 	m3()
 	{
 		System.out.println("this is m3 method");
+		System.out.println("program for check pull method");
+	}
 	}
 	
 
