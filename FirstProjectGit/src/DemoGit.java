3c1688ffdf87314c686a336d2e27e19d24fff30a
spadole1 <99403235+spadole1@users.noreply.github.com>
2022-02-11 09:20:41 +0530

    by master

--- FirstProjectGit/src/DemoGit.java
+++ FirstProjectGit/src/DemoGit.java
@@ -1,9 +1,14 @@
 
 public class DemoGit {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
     System.out.println("First Program for GitHub");
+	}
+	m2()
+	{
+		System.out.println("program for m2 method");
 	}
 
 	
